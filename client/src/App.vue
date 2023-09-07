a2da1c4f2ce80168d27a716f8d8e243fdfcdd29f
Johan Sandgren <gussanjoba@student.gu.se>
2023-09-07 13:52:48 +0200

    bootstrapvue tests

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,66 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
+    <b-navbar toggleable="lg" type="light" variant="light">
+      <b-navbar-brand to="/">LOGO</b-navbar-brand>
+
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav>
+          <b-nav-item class="test" to="/dashboard">Dashboard</b-nav-item>
+          <b-nav-item to="/meetings">Meetings</b-nav-item>
+          <b-nav-item to="/tasks">Tasks</b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+
+      <b-navbar-nav class="ml-auto">
+        <b-nav-item>Username</b-nav-item>
+      </b-navbar-nav>
+    </b-navbar>
+
+    <div class="main">
+      <div class="section">
+        <h1 class="section-title">Assignment Group</h1>
+        <div class="section-content-1">
+          <b-card title="Card Title" img-alt="Image" img-top>
+            <b-card-text>
+              asdfasdf
+            </b-card-text>
+          </b-card>
+          <b-card title="Card Title" img-alt="Image" img-top>
+            <b-card-text>
+              asdf
+            </b-card-text>
+          </b-card>
+          <b-card title="Card Title" img-alt="Image" img-top>
+            <b-card-text>
+              ff
+            </b-card-text>
+          </b-card>
+        </div>
+      </div>
+      <div class="section">
+        <h1 class="section-title">Scheduled Meetings</h1>
+        <div class="section-content-2">
+          <div class="meeting-item">
+            <p class="course">Software Arch A1 </p>
+            <span class="rightside">
+              <p class="label">ZOOM</p>
+              <p class="date">3 Aug 13.00</p>
+            </span>
+          </div>
+          <div class="meeting-item">
+            <p class="course">Software Arch A1 </p>
+            <span class="rightside">
+              <p class="label">ZOOM</p>
+              <p class="date">3 Aug 13.00</p>
+            </span>
+          </div>
+        </div>
+      </div>
+
+      <div class="section">sec</div>
     </div>
-    <!-- Render the content of the current page view -->
-    <router-view/>
   </div>
 </template>
 
@@ -13,7 +69,61 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+}
+
+.main {
+  background-color: #2c3e50;
+  display: flex;
+  flex-direction: column;
+}
+
+.section {
+  color: blueviolet;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+}
+
+.section-title {
+  color: white;
+  font-size: 2em;
+  margin: 0;
+}
+
+.section-content-1 {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  padding-top: 6px;
+  grid-gap: 10px;
+}
+
+.section-content-2 {
+  display: flex;
+  flex-direction: column;
+
+}
+
+.meeting-item {
+  background-color: turquoise;
+  color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+}
+
+.label {
+  color: black;
+  background-color: lightgreen;
+  padding: 5px;
+  border-radius: 50px;
+  margin-right: 10px;
+
+}
+
+.rightside {
+  display: flex;
+
+  justify-content: space-between;
 }
 </style>
