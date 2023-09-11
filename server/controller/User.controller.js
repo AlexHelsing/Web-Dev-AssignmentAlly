66e894ee85d138e3cb0257b50b19f90f6847fe0d
Alexander <helsinga@student.chalmers.se>
2023-09-11 21:58:34 +0200

    Remove auth router and cleanup

--- server/controller/User.controller.js
+++ server/controller/User.controller.js
@@ -33,32 +33,6 @@ async function deleteUser(req, res){
 }
 
 
-//create a new user with username and some random password
-async function registerUser(req, res) {
-  const user = new User({
-    username: req.body.username,
-    password: req.body.password
-  });
-  try {
-    let hash = await bcrypt.hash(req.body.password, 10);
-    user.password = hash;
-    const newUser = await user.save();
-    res.status(201).json(newUser);
-  } catch (err) {
-    res.status(400).json({ message: err.message});
-  }
-}
-
-async function loginUser(req, res){
-  try{
-
-  }
-  catch(err){
-    res.status(400).json({message: err.message})
-  }
-}
-
-
 module.exports = {
   getAllUsers,
   registerUser,
---
+++ server/routes/Auth.router.js
@@ -0,0 +1,9 @@
+const AuthRouter = require('express').Router();
+
+const AuthController = require('../controller/Auth.controller');
+
+//UserRouter.get('/getAllUsers', UserController.getAllUsers);
+//UserRouter.post('/registerUser', UserController.registerUser);
+
+
+module.exports = AuthRouter;
