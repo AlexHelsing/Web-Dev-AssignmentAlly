0d84426e2e3803d9a4bccbe2a87076eb23fed8a4
Alexander <helsinga@student.chalmers.se>
2023-09-17 22:10:48 +0200

    Fix router

--- server/routes/GroupRouter.js
+++ server/routes/GroupRouter.js
@@ -3,7 +3,7 @@ const GroupRouter = require('express').Router();
 const GroupController = require('../controller/Group.controller');
 
 GroupRouter.post('/create-group', GroupController.createGroup);
-GroupRouter.post('/getAllGroups', GroupController.getAllGroups);
-GroupRouter.post('/:course', GroupController.getGroup);
+GroupRouter.get('/getAllGroups', GroupController.getAllGroups);
+GroupRouter.get('/:course', GroupController.getGroup);
 
 module.exports = GroupRouter;
