853779b7051953661167123d513a7364ddecb2c7
Alexander <helsinga@student.chalmers.se>
2023-09-17 22:18:33 +0200

    Add better delete user functionality

--- server/routes/User.router.js
+++ server/routes/User.router.js
@@ -5,7 +5,7 @@ const UserController = require('../controller/User.controller');
 
 UserRouter.get('/getAllUsers', UserController.getAllUsers);
 UserRouter.get('/:username', UserController.getUser);
-UserRouter.post('/deleteUser', UserController.deleteUser),
+UserRouter.post('/:username/deleteUser', UserController.deleteUser),
 UserRouter.post('/:username/changePassword', UserController.changePassword),
 
 module.exports = UserRouter;
