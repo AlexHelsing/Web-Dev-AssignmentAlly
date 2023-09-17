0764044cb1e12c55f07d0c94fef3696614429221
Alexander <helsinga@student.chalmers.se>
2023-09-17 22:23:22 +0200

    Add PATCH functionality instead of POST for changing password

--- server/routes/User.router.js
+++ server/routes/User.router.js
@@ -6,6 +6,6 @@ const UserController = require('../controller/User.controller');
 UserRouter.get('/getAllUsers', UserController.getAllUsers);
 UserRouter.get('/:username', UserController.getUser);
 UserRouter.post('/:username/deleteUser', UserController.deleteUser),
-UserRouter.post('/:username/changePassword', UserController.changePassword),
+UserRouter.patch('/:username/changePassword', UserController.changePassword),
 
 module.exports = UserRouter;
