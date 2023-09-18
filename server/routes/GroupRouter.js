3a77a489392fb974f7d7f41b00054464f214df22
Alexander <helsinga@student.chalmers.se>
2023-09-18 18:01:25 +0200

    Change REST things like put, post

--- server/routes/GroupRouter.js
+++ server/routes/GroupRouter.js
@@ -2,8 +2,8 @@ const GroupRouter = require('express').Router();
 
 const GroupController = require('../controller/Group.controller');
 
-GroupRouter.post('/create-group', GroupController.createGroup);
-GroupRouter.post('/:course/delete-group', GroupController.deleteGroup);
+GroupRouter.put('/create-group', GroupController.createGroup);
+GroupRouter.delete('/:course/delete-group', GroupController.deleteGroup);
 GroupRouter.get('/getAllGroups', GroupController.getAllGroups);
 GroupRouter.get('/:course', GroupController.getGroup);
 
--- server/routes/User.router.js
+++ server/routes/User.router.js
@@ -5,7 +5,7 @@ const UserController = require('../controller/User.controller');
 
 UserRouter.get('/getAllUsers', UserController.getAllUsers);
 UserRouter.get('/:username', UserController.getUser);
-UserRouter.post('/:username/deleteUser', UserController.deleteUser),
+UserRouter.delete('/:username/deleteUser', UserController.deleteUser),
 UserRouter.patch('/:username/changePassword', UserController.changePassword),
 
 module.exports = UserRouter;
