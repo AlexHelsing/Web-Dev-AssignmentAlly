270672f7980f85b7110f78871f40f8c6c82edd49
Johan Sandgren <gussanjoba@student.gu.se>
2023-10-17 03:42:17 +0200

    localhost to 127.0.01

--- server/tests/dropdb.js
+++ server/tests/dropdb.js
@@ -4,19 +4,19 @@ var mongoose = require('mongoose');
 var mongoURI = process.env.MONGODB_URI;
 
 if (!mongoURI) {
-    console.error('Missing MONGODB_URI for dropping test database.');
-    process.exit(1);
+  console.error('Missing MONGODB_URI for dropping test database.');
+  process.exit(1);
 }
 
 // Drop database
 mongoose.connect(mongoURI).catch(function (err) {
-    if (err) {
-        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
-        console.error(err.stack);
-        process.exit(1);
-    }
+  if (err) {
+    console.error(`Failedd to connect to MongoDB with URI: ${mongoURI}`);
+    console.error(err.stack);
+    process.exit(1);
+  }
 });
 mongoose.connection.dropDatabase().then(function () {
-    console.log(`Dropped database: ${mongoURI}`);
-    process.exit(0);
+  console.log(`Dropped database: ${mongoURI}`);
+  process.exit(0);
 });
